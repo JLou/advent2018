dbf637d07bb286f43768192bd4414650688b1ad0
JLou <jlou.xd@gmail.com>
2018-12-02 19:43:35 +0100

    day1

---
+++ day1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string line;
+            HashSet<int> values = new HashSet<int>();
+            bool found = false;
+            bool firstLoop = true;
+            while(!found)
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(@"input.txt");  
+                while((line = file.ReadLine()) != null)  
+                {
+                    if(!values.Add(count))
+                    {
+                        Console.WriteLine("repeated: " + count);
+                        found = true;
+                        break;
+                    }
+                    count += int.Parse(line);  
+                }  
+                if(firstLoop){
+                    Console.WriteLine(count);    
+                    firstLoop = false;
+                }
+            }
+            
+
+
+            Console.Read();
+        }
+    }
+}
