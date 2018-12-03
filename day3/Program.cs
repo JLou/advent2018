c9b8768a93dfe8a6ac39d5b4a41f9bbf25f60d6b
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2018-12-03 13:18:50 +0100

    day3

---
+++ day3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            using (var file = new System.IO.StreamReader(@"input.txt"))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    //Part 1
+
+
+                    //Part 2
+                }
+
+                file.Close();
+            }
+        }
+    }
+}
