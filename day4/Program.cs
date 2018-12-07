d294f82a8c53aa2b6bec3397d9218e42a07af567
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2018-12-07 17:21:29 +0100

    day3/4/5

---
+++ day4/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            SortedList<DateTime, Input> inputs = new SortedList<DateTime, Input>();
+
+            using (var file = new System.IO.StreamReader(@"input.txt"))
+            {
+                Regex r = new Regex(@"\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (.+)");
+                int currentGuard = 0;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var m = r.Match(line);
+                    var date = new DateTime(
+                            int.Parse(m.Groups[1].Value),
+                            int.Parse(m.Groups[2].Value),
+                            int.Parse(m.Groups[3].Value),
+                            int.Parse(m.Groups[4].Value),
+                            int.Parse(m.Groups[5].Value),
+                            0
+                        );
+
+                    Input input = new Input();
+                    switch (m.Groups[6].Value[0])
+                    {
+                        case 'G':
+                            input.Action = Action.NewGuard;
+                            currentGuard = int.Parse(m.Groups[6].Value.Split(' ')[1].Substring(1));
+                            break;
+                        case 'f':
+                            input.Action = Action.Asleep;
+                            break;
+                        case 'w':
+                            input.Action = Action.WakesUp;
+                            break;
+                    }
+                    input.GuardId = currentGuard;
+                    inputs.Add(date, input);
+                }
+            }
+            Dictionary<int, int> totalSlept = new Dictionary<int, int>();
+            Dictionary<int, int[]> hoursSlept = new Dictionary<int, int[]>();
+
+            int startSleepMinute = 0;
+            int guardId = 0;
+            foreach (var input in inputs)
+            {
+
+                if (input.Value.Action == Action.NewGuard)
+                {
+                    guardId = input.Value.GuardId;
+                }
+                if (!totalSlept.ContainsKey(guardId))
+                {
+                    totalSlept.Add(guardId, 0);
+                    hoursSlept.Add(guardId, new int[60]);
+                }
+                if (input.Value.Action == Action.Asleep)
+                {
+                    startSleepMinute = input.Key.Minute;
+                }
+
+                if (input.Value.Action == Action.WakesUp)
+                {
+                    var totalMinutes = input.Key.Minute - startSleepMinute;
+                    totalSlept[guardId] += totalMinutes;
+                    for (int i = 0; i < totalMinutes; i++)
+                    {
+                        hoursSlept[guardId][i + startSleepMinute]++;
+                    }
+                }
+            }
+
+            var lazyGuy = totalSlept.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+            int maxValue = hoursSlept[lazyGuy].Max();
+            int maxIndex = hoursSlept[lazyGuy].ToList().IndexOf(maxValue);
+            Console.WriteLine("answer1: " + lazyGuy * maxIndex);
+
+            (int guard2, int minute, int total) = (0, 0, 0);
+            foreach (var (guardid, schedule) in hoursSlept)
+            {
+                int maxValue2 = schedule.Max();
+
+                if (total < maxValue2)
+                {
+                    minute = schedule.ToList().IndexOf(maxValue2);
+                    total = maxValue2;
+                    guard2 = guardid;
+                }
+            }
+
+            Console.WriteLine("answer2: " + guard2 * minute);
+        }
+
+        class Input
+        {
+            public Action Action { get; set; }
+
+            public int GuardId { get; set; }
+        }
+
+        private enum Action
+        {
+            NewGuard = 1,
+            Asleep = 2,
+            WakesUp = 3
+        }
+    }
+}
---
+++ day5/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line, originalline;
+            using (var file = new System.IO.StreamReader(@"input.txt"))
+            {
+                line = file.ReadLine();
+            }
+            originalline = line;
+            //part1
+            if (false)
+            {
+                ReactPolymer(line);
+            }
+            else
+            {
+                int minSize = int.MaxValue;
+
+                for (int c = 'a'; c <= 'z'; c++)
+                {
+                    var str = originalline.Replace(((char)c).ToString(), string.Empty);
+                    str = str.Replace((Char.ToUpper((char)c)).ToString(), string.Empty);
+                    var size = ReactPolymer(str);
+
+
+                    minSize = size < minSize ? size : minSize;
+                }
+
+                Console.WriteLine("min polymer: " + minSize);
+            }
+        }
+
+        private static int ReactPolymer(string polymer)
+        {
+            StringBuilder sb = new StringBuilder(polymer);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (i == sb.Length - 1)
+                {
+                    break;
+                }
+                if (!(Char.ToLower(sb[i]) == Char.ToLower(sb[i + 1]) && sb[i] != sb[i + 1]))
+                {
+                }
+                else
+                {
+                    sb.Remove(i, 2);
+                    i -= 2;
+                    if (i < -1)
+                    {
+                        i = -1;
+                    }
+                }
+            }
+
+
+            return sb.Length;
+        }
+    }
+}
